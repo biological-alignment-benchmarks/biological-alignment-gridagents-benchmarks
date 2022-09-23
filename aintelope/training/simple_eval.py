b3b9220a4a219a77d9ac5a24a195d65bad678d53
Nathan Helm-Burger <nathan.helm.burger@gmail.com>
2022-09-22 19:08:00 -0700

    fleshing out simple_eval for use in agent tests

--- aintelope/training/simple_eval.py
+++ aintelope/training/simple_eval.py
@@ -1,4 +1,5 @@
 import random
+import os
 from pprint import pprint
 import numpy as np
 from gym.spaces import Discrete
@@ -11,52 +12,90 @@ from aintelope.agents.simple_agents import (
     IterativeWeightOptimizationAgent,
 )
 from aintelope.agents.memory import ReplayBuffer
-from aintelope.aintelope.environments.savanna_zoo import (
-    env,
-    move_agent,
-    reward_agent,
-)
+from aintelope.models.dqn import DQN
+from aintelope.aintelope.environments.savanna_zoo import SavannaZooEnv
+from aintelope.aintelope.environments.savanna_gym import SavannaGymEnv
 
-import os
+# from aintelope.aintelope.environments.savanna_zoo import (
+#     env,
+#     move_agent,
+#     reward_agent,
+# )
 
 
-def run_episode(env):
-    mode = "ascii"
-    # policy = IterativeWeightOptimizationAgent()
-    # policy = OneStepPerfectPredictionAgent()
-    # policy = RandomWalkAgent()
 
-    replay_size = 1000
+# is there a better way to do this? 
+# to register a lookup table from hparam name to function?
+AGENT_LOOKUP = {
+    'q_agent': Qagent,
+    'shard_agent': ShardAgent,
+    'random_walk_agent': RandomWalkAgent,
+    'one_step_perfect_prediction_agent': OneStepPerfectPredictionAgent,
+    'iterative_weight_optimization_agent': IterativeWeightOptimizationAgent
+}
+
+ENV_LOOKUP = {
+    'savanna_zoo_env': SavannaZooEnv
+    
+}
+
+MODEL_LOOKUP = {
+    'dqn': DQN
+}
+
+
+def run_episode(hparams: dict = {}):
+    env_params = hparams.get('env_params', {})
+    agent_params = hparams.get('agent_params', {})
+    render_mode = hparams.get('render_mode')
+    verbose = hparams.get('verbose', False)
+
+    if env_params.get('env_type') == 'zoo':
+            env = ENV_LOOKUP[env_params['name']](env_params=env_params)
+            obs_size = env.observation_space.shape[0]
+    elif env_params.get('env_type') == 'gym':
+        # GYM_INTERACTION
+        if hparams.get('env_entry_point') is not None:
+            gym.envs.register(
+                id=env_params['name'],
+                entry_point=hparams['env_entry_point'], # e.g. 'aintelope.environments.savanna_gym:SavannaGymEnv'
+                kwargs={'env_params': env_params}
+            )
+        env = gym.make(hparams['env'])
+        obs_size = env.observation_space.shape[0]
+    else:
+        print(f'env_type {env_params.get("env_type")} not implemented. Choose: [zoo, gym]')
+    
     env.reset()
-    replay_buffer = ReplayBuffer(replay_size)
-    policy = Q_agent(gym.make(env), replay_buffer)
-    for agent in env.agent_iter():
-        observation, reward, done, info = env.last()
+    n_actions = env.action_space.n
 
-        action_space: Discrete = env.action_space(agent)
-        if not done:
-            action = policy(action_space, observation, reward, info)
-            assert action in action_space
-        else:
-            action = None
-        env.step(action)
-        env.render(mode)
+    net = MODEL_LOOKUP[hparams['model']](obs_size, n_actions)
 
-    # wait = input("Close?")
+    buffer = ReplayBuffer(hparams.replay_size)
+    agent = AGENT_LOOKUP[hparams['agent']](env, buffer, **agent_params)
+    episode_reward = 0
 
+    done = False
+    warm_start_steps = hparams.get('warm_start_steps', 1000)
+    for i in range(warm_start_steps):
+        reward, done = agent.play_step(net, epsilon=1.0)
+        if done:
+            if verbose: 
+                print(f'Uhoh! Your agent terminated during warmup on step {i}/{warm_start_steps}')
+            break
 
-if __name__ == "__main__":
-    env_params = {
-        "NUM_ITERS": 40,  # duration of the game
-        "MAP_MIN": 0,
-        "MAP_MAX": 20,
-        "render_map_max": 20,
-        "AMOUNT_AGENTS": 1,  # for now only one agent
-        "AMOUNT_GRASS_PATCHES": 2,
-    }
-    e = env(env_params=env_params)
-    main(e)
-
-# Local Variables:
-# compile-command: "poetry run python simple.py"
-# End:
+    while done is False:
+        # step through environment with agent
+        epsilon = max(
+                hparams['eps_end'],
+                hparams['eps_start']
+                - env.num_moves * 1 / hparams['eps_last_frame'],
+            )
+         
+        reward, done = agent.play_step(net, epsilon, hparams.get('device', 'cuda'))
+        episode_reward += reward
+        if render_mode is not None:
+            env.render(render_mode)
+            
+    if verbose:
+        print(f'Simple Episode Evaluation completed. Final episode reward: {episode_reward}')
--- tests/agents/test_q_agent.py
+++ tests/agents/test_q_agent.py
@@ -1,4 +1,19 @@
 import pytest
 
+from aintelope.aintelope.environments.savanna_zoo import SavannaZooEnv
 
+def test_qagent_in_savanna_zoo_sequential():
+    # TODO: refactor out into test constants? Or leave here? /shrug
+    env_params = {
+    "NUM_ITERS": 40,  # duration of the game
+    "MAP_MIN": 0,
+    "MAP_MAX": 20,
+    "render_map_max": 20,
+    "AMOUNT_AGENTS": 1,  # for now only one agent
+    "AMOUNT_GRASS_PATCHES": 2,
+    }
+    e = SavannaZooEnv(env_params=env_params)
+    
+    
 
+    
