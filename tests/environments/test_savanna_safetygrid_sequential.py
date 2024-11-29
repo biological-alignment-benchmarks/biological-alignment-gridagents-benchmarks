91fc639e89615cc54fc25d9e3057f0dd56073d08
Roland Pihlakas <roland@simplify.ee>
2024-11-29 03:35:42 +0200

    Change to unit tests to reflect the environments' change from MultiDiscrete to Discrete action space.

--- tests/environments/test_savanna_safetygrid_sequential.py
+++ tests/environments/test_savanna_safetygrid_sequential.py
@@ -185,7 +185,7 @@ def test_gridworlds_action_spaces():
     env = safetygrid.SavannaGridworldSequentialEnv()
 
     for agent in env.possible_agents:
-        assert isinstance(env.action_space(agent), MultiDiscrete)
+        assert isinstance(env.action_space(agent), Discrete)
         assert env.action_space(agent).n == 5  # includes no-op
 
 
